bc834d6315a0408c5504aec446f127ba7a46f1f4
lifanyuan <1030426634@qq.com>
2018-11-19 11:26:00 +0800

    dual voice simulation success

--- dsd_pcm.v
+++ dsd_pcm.v
@@ -26,7 +26,8 @@ module dsd_pcm(
     input ws,
     input rst_n,
 //    output data_out_end_L_1
-    output modulator_out_L
+    output modulator_out_L,
+    output modulator_out_R////////////////////////////////
     );
 // assign data_out_end_L_1=modulator_out_L;
  wire en_64,en_32,en_16,en_8;
@@ -38,18 +39,21 @@ module dsd_pcm(
                          .en_8(en_8)
                   );   
 wire[31:0]iis_pcm_out_L;
-wire[31:0]iis_pcm_out_L_1;
 wire[23:0]iis_pcm_out_L_2;
+wire[31:0]iis_pcm_out_R;///////////////////////
+wire[23:0]iis_pcm_out_R_2;
 iis_pcm iis_pcm(        .bclk(bclk),
                         .clk(bclk),
                         .ws(ws),
                         .data_in(data_in),
                         .rst_n(rst_n),
-                        .enable(en_64),
-                        .data_out_L(iis_pcm_out_L)
+                        .enable(en_32),
+                        .data_out_L(iis_pcm_out_L),
+                        .data_out_R(iis_pcm_out_R)
                    );                  
 assign iis_pcm_out_L_2=iis_pcm_out_L[31:8];
-wire [23:0]deal_pcm_out_L;
+assign iis_pcm_out_R_2=iis_pcm_out_R[31:8];
+wire [23:0]deal_pcm_out_L,deal_pcm_out_R;
 deal_pcm deal_pcm_L(  .in(iis_pcm_out_L_2),//({iis_pcm_out_L_2[23],iis_pcm_out_L_2[23:1]}),
                         .clk(bclk),
                        .rst_n(rst_n),
@@ -58,10 +62,24 @@ deal_pcm deal_pcm_L(  .in(iis_pcm_out_L_2),//({iis_pcm_out_L_2[23],iis_pcm_out_L
                        .en_8(en_8),
                        .out(deal_pcm_out_L)
                   );
+deal_pcm deal_pcm_R(  .in(iis_pcm_out_R_2),//({iis_pcm_out_L_2[23],iis_pcm_out_L_2[23:1]}),
+                          .clk(bclk),
+                         .rst_n(rst_n),
+                         .en_32(en_32),
+                         .en_16(en_16),
+                         .en_8(en_8),
+                         .out(deal_pcm_out_R)
+                                    );
 modulator_512 modulator_512_L( .in(deal_pcm_out_L),
                                .out(modulator_out_L),
                                .rst_n(rst_n),
                                .clk(bclk),
                                .enable(1'b1)
                              );    
+modulator_512 modulator_512_R( .in(deal_pcm_out_R),
+                            .out(modulator_out_R),
+                            .rst_n(rst_n),
+                            .clk(bclk),
+                            .enable(1'b1)
+                          );    
 endmodule
--- iis_pcm.v
+++ iis_pcm.v
@@ -1,4 +1,4 @@
-module iis_pcm(bclk,clk,ws,data_in,rst_n,enable,data_out_L);
+module iis_pcm(bclk,clk,ws,data_in,rst_n,enable,data_out_L,data_out_R);
 
 input bclk;
 input clk;
@@ -8,6 +8,7 @@ input enable;
 input rst_n;
 
 output [31:0]data_out_L;
+output [31:0]data_out_R;
 
 wire[31:0]data_out_L;
 wire[31:0]data_out_R;
@@ -40,9 +41,14 @@ begin
 	 end
 end
 //***********************************
-reg [5:0]cnt_L;
-reg cnt_en_L;
-always @(posedge bclk or negedge rst_n)
+reg [5:0]cnt_L,cnt_R;
+reg cnt_en_L,cnt_en_R;
+reg rd_en_L,rd_en_R;
+reg wr_en_L,wr_en_R;
+wire [5:0]wrusedw_L,wrusedw_R;
+wire [5:0]rdusedw_L,rdusedw_R;
+/*************left*********************/
+always @(posedge bclk or negedge rst_n)//generate a pulse at any time when ws_1=1
 begin
   if(!rst_n)
   cnt_L<=0;
@@ -60,39 +66,11 @@ begin
   else
   cnt_en_L<=0;
 end
-//*****************************************
-reg rd_en_L;
-reg wr_en_L;
-wire [5:0]wrusedw_L;
-wire [5:0]rdusedw_L;
-//fifo_pcm_L fifo_L(  .aclr(~rst_n),
-//                .data(data_L_temp_1),
-//	             .rdclk(clk),
-//	             .rdreq(enable&rd_en_L),
-//	             .wrclk(bclk),
-//	             .wrreq(cnt_en_L&wr_en_L),
-//	             .q(data_out_L),
-//	             .wrusedw(wrusedw_L),
-//					 .rdusedw(rdusedw_L)
-//				 );
-fifo_pcm_L your_instance_name (
-                   .rst(~rst_n),                      // input wire rst
-                   .wr_clk(bclk),                // input wire wr_clk
-                   .rd_clk(clk),                // input wire rd_clk
-                   .din(data_L_temp_1),                      // input wire [31 : 0] din
-                   .wr_en(cnt_en_L&wr_en_L),                  // input wire wr_en
-                   .rd_en(enable&rd_en_L),                  // input wire rd_en
-                   .dout(data_out_L),                    // output wire [31 : 0] dout
-                   .full(),                    // output wire full
-                   .empty(),                  // output wire empty
-                   .rd_data_count(rdusedw_L),  // output wire [5 : 0] rd_data_count
-                   .wr_data_count(wrusedw_L)  // output wire [5 : 0] wr_data_count
-                 );
 always @(posedge clk or negedge rst_n)
 begin
   if(!rst_n) 
   rd_en_L<=0;
-  else if(rdusedw_L>=6'd10)
+  else if(rdusedw_L>=6'd1)
   rd_en_L<=1;
   else 
   rd_en_L<=0;
@@ -106,6 +84,81 @@ begin
   wr_en_L<=0;
   else
   wr_en_L<=1;
-end  
+end 
+/**************right*******************/
+always @(posedge bclk or negedge rst_n)//generate a pulse at any time when ws_1=0
+begin
+  if(!rst_n)
+  cnt_R<=0;
+  else if(ws_1==0)
+  cnt_R<=cnt_R+1;
+  else if(ws_1==1)
+  cnt_R<=0;
+end
+always @(posedge bclk or negedge rst_n)
+begin
+  if(!rst_n)
+  cnt_en_R<=0;
+  else if(cnt_R==6'd15)
+  cnt_en_R<=1;
+  else
+  cnt_en_R<=0;
+end
+always @(posedge clk or negedge rst_n)
+begin
+  if(!rst_n) 
+  rd_en_R<=0;
+  else if(rdusedw_R>=6'd1)
+  rd_en_R<=1;
+  else 
+  rd_en_R<=0;
+end
+always @(posedge bclk or negedge rst_n)
+begin
+  if(!rst_n)
+  wr_en_R<=0;
+  else if(wrusedw_R>=6'd32)
+  wr_en_R<=0;
+  else
+  wr_en_R<=1;
+end 
+
+//fifo_pcm_L fifo_L(  .aclr(~rst_n),
+//                .data(data_L_temp_1),
+//	             .rdclk(clk),
+//	             .rdreq(enable&rd_en_L),
+//	             .wrclk(bclk),
+//	             .wrreq(cnt_en_L&wr_en_L),
+//	             .q(data_out_L),
+//	             .wrusedw(wrusedw_L),
+//					 .rdusedw(rdusedw_L)
+//				 );
+fifo_pcm_L fifo_L (
+                   .rst(~rst_n),                      // input wire rst
+                   .wr_clk(bclk),                // input wire wr_clk
+                   .rd_clk(clk),                // input wire rd_clk
+                   .din(data_L_temp_1),                      // input wire [31 : 0] din
+                   .wr_en(cnt_en_L&wr_en_L),                  // input wire wr_en
+                   .rd_en(enable&rd_en_L),                  // input wire rd_en
+                   .dout(data_out_L),                    // output wire [31 : 0] dout
+                   .full(),                    // output wire full
+                   .empty(),                  // output wire empty
+                   .rd_data_count(rdusedw_L),  // output wire [5 : 0] rd_data_count
+                   .wr_data_count(wrusedw_L)  // output wire [5 : 0] wr_data_count
+                 );
+fifo_pcm_L fifo_R (
+                    .rst(~rst_n),                      // input wire rst
+                    .wr_clk(bclk),                // input wire wr_clk
+                    .rd_clk(clk),                // input wire rd_clk
+                    .din(data_R_temp_1),                      // input wire [31 : 0] din
+                    .wr_en(cnt_en_R&wr_en_R),                  // input wire wr_en
+                    .rd_en(enable&rd_en_R),                  // input wire rd_en
+                    .dout(data_out_R),                    // output wire [31 : 0] dout
+                    .full(),                    // output wire full
+                    .empty(),                  // output wire empty
+                    .rd_data_count(rdusedw_R),  // output wire [5 : 0] rd_data_count
+                    .wr_data_count(wrusedw_R)  // output wire [5 : 0] wr_data_count
+                                  );
+ 
 
 endmodule
